77f0f50693023a55b7ffd52e2198b7bd49b5185d
jagadishck018 <jagadishck.018@gmail.com>
2025-04-26 22:56:33 +0530

    feat: add LoginPage component and update NavBar routing to point to LoginPage

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import React from "react";
 
 import PostList from "./posts/page";
 import NavBar from "@/components/navigation-bar/Navbar";
-import LoginPage from "@/components/navigation-bar/Login";
+
 
 const RootPage = () => {
   return (
     <div className=" mx-auto max-w-7xl  bg-[#F6F6EF] h-screen">
       <NavBar />
-      <LoginPage />
+      
       <div className=" ">
         <PostList />
       </div>
