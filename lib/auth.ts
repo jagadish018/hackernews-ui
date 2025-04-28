474ee7db9ee82e7441410c965bb5d8a52c88a62c
jagadishck018 <jagadishck.018@gmail.com>
2025-04-29 01:30:40 +0530

    implement NavigationBar and SectionWrapper components; update auth client baseURL for local development

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,9 @@
 // lib/auth.ts
-
+import { usernameClient } from "better-auth/client/plugins";
 import { nextCookies } from "better-auth/next-js";
 import { createAuthClient } from "better-auth/react";
 
 export const betterAuthClient = createAuthClient({
-  baseURL:
-    "https://hackernews.agreeablesmoke-a4d23e0d.centralindia.azurecontainerapps.io",
-  plugins: [nextCookies()],
+  baseURL: "http://localhost:3000",
+  plugins: [nextCookies(), usernameClient()],
 });
