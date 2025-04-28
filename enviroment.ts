8cfb6dd5df0af35fbc0e70fb10a499bb7cf03f26
jagadishck018 <jagadishck.018@gmail.com>
2025-04-29 02:09:26 +0530

    fix: update baseURL in environment and auth client to production endpoint

--- enviroment.ts
+++ enviroment.ts
@@ -1,1 +1,2 @@
-export const url = "http://localhost:3000";
+export const url =
+  "https://hackernews.agreeablesmoke-a4d23e0d.centralindia.azurecontainerapps.io";
--- lib/auth.ts
+++ lib/auth.ts
@@ -4,6 +4,7 @@ import { nextCookies } from "better-auth/next-js";
 import { createAuthClient } from "better-auth/react";
 
 export const betterAuthClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL:
+    "https://hackernews.agreeablesmoke-a4d23e0d.centralindia.azurecontainerapps.io",
   plugins: [nextCookies(), usernameClient()],
 });
