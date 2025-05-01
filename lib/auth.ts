b8a50e2f2bac98a9673c2b5f46f6639a7ad86d51
jagadishck018 <jagadishck.018@gmail.com>
2025-05-01 12:36:33 +0530

    refactor: update serverUrl to use environment variable and simplify dev script

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,11 @@
 // lib/auth.ts
+import { serverUrl } from "@/enviroment";
 import { usernameClient } from "better-auth/client/plugins";
 import { nextCookies } from "better-auth/next-js";
 import { createAuthClient } from "better-auth/react";
 
 export const betterAuthClient = createAuthClient({
-  baseURL:
-    "https://hackernews.agreeablesmoke-a4d23e0d.centralindia.azurecontainerapps.io",
+  baseURL:serverUrl,
   plugins: [usernameClient(), nextCookies()],
   
 });
