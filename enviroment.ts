6c995037717735c6bd564ffc551a4205b6e4c084
jagadishck018 <jagadishck.018@gmail.com>
2025-05-01 12:54:02 +0530

    fix: remove trailing space in serverUrl environment variable assignment

--- enviroment.ts
+++ enviroment.ts
@@ -1,3 +1,3 @@
 // export const serverUrl = "https://hackernews.agreeablesmoke-a4d23e0d.centralindia.azurecontainerapps.io";
 
-export const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL ;
+export const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
